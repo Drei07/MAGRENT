3f8c02855c8f5d8defc0c30a830c4258906abfdf
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2023-11-23 22:30:21 +0800

    MAIN

---
+++ database/magrent.sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 23, 2023 at 03:29 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `magrent`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_config`
+--
+
+CREATE TABLE `email_config` (
+  `Id` int(145) NOT NULL,
+  `email` varchar(145) DEFAULT NULL,
+  `password` varchar(145) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `email_config`
+--
+
+INSERT INTO `email_config` (`Id`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'ecket2023@gmail.com', 'vnvbophnoxickore\n', '2023-02-20 03:25:24', '2023-07-12 02:41:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `google_recaptcha_api`
+--
+
+CREATE TABLE `google_recaptcha_api` (
+  `Id` int(11) NOT NULL,
+  `site_key` varchar(145) DEFAULT NULL,
+  `site_secret_key` varchar(145) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `google_recaptcha_api`
+--
+
+INSERT INTO `google_recaptcha_api` (`Id`, `site_key`, `site_secret_key`, `created_at`, `updated_at`) VALUES
+(1, '6LdiQZQhAAAAABpaNFtJpgzGpmQv2FwhaqNj2azh', '6LdiQZQhAAAAAByS6pnNjOs9xdYXMrrW2OeTFlrm', '2023-02-19 16:57:18', '2023-08-12 16:48:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `activity` varchar(145) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_config`
+--
+
+CREATE TABLE `system_config` (
+  `id` int(14) NOT NULL,
+  `system_name` varchar(145) DEFAULT NULL,
+  `system_phone_number` varchar(145) DEFAULT NULL,
+  `system_email` varchar(145) DEFAULT NULL,
+  `system_logo` varchar(145) DEFAULT NULL,
+  `system_favicon` varchar(145) DEFAULT NULL,
+  `system_address` varchar(145) DEFAULT NULL,
+  `system_facebook` varchar(145) DEFAULT NULL,
+  `system_instagram` varchar(145) DEFAULT NULL,
+  `system_copy_right` varchar(145) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_config`
+--
+
+INSERT INTO `system_config` (`id`, `system_name`, `system_phone_number`, `system_email`, `system_logo`, `system_favicon`, `system_address`, `system_facebook`, `system_instagram`, `system_copy_right`, `created_at`, `updated_at`) VALUES
+(1, 'MAGRENT', '+639126842485', 'magrentofficial@gmail.com', 'logo.png', 'favicon.ico', 'Brgy. San Isidro, Safragemc Village, San Francisco, Agusan Del Sur, 8501', 'https://www.facebook.com/rhey.timario', 'https://www.facebook.com/rhey.timario', 'COPYRIGHT © 2023 - MAGRENT. ALL RIGHTS RESERVED.', '2023-11-19 13:03:25', '2023-11-19 13:10:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(145) DEFAULT NULL,
+  `middle_name` varchar(145) DEFAULT NULL,
+  `last_name` varchar(145) DEFAULT NULL,
+  `sex` varchar(145) DEFAULT NULL COMMENT 'male=1, female=2',
+  `date_of_birth` varchar(145) DEFAULT NULL,
+  `age` varchar(145) DEFAULT NULL,
+  `civil_status` varchar(145) DEFAULT NULL,
+  `phone_number` varchar(145) DEFAULT NULL,
+  `email` varchar(145) DEFAULT NULL,
+  `password` varchar(145) DEFAULT NULL,
+  `profile` varchar(1145) NOT NULL DEFAULT 'profile.png',
+  `valid_id` varchar(1500) DEFAULT NULL,
+  `status` enum('Y','N') DEFAULT 'N',
+  `tokencode` varchar(145) DEFAULT NULL,
+  `account_status` enum('active','disabled') NOT NULL DEFAULT 'active',
+  `user_type` varchar(14) DEFAULT NULL COMMENT 'superadmin=0,\r\nadmin=1,\r\nsub-admin=2,\r\nuser=3',
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `middle_name`, `last_name`, `sex`, `date_of_birth`, `age`, `civil_status`, `phone_number`, `email`, `password`, `profile`, `valid_id`, `status`, `tokencode`, `account_status`, `user_type`, `created_at`, `updated_at`) VALUES
+(1, 'SUPERADMIN', 'SUPERADMIN', 'SUPERADMIN', 'MALE', NULL, NULL, NULL, NULL, 'andrei.m.viscayno@gmail.coms', '24b35e91f6650c460b66bceaa1590664', 'profile.png', NULL, 'Y', '174b66675b0e6170e83b8f4fbd7ba02f', 'active', '2', '2023-11-20 04:14:08', '2023-11-23 12:50:11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `email_config`
+--
+ALTER TABLE `email_config`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `google_recaptcha_api`
+--
+ALTER TABLE `google_recaptcha_api`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_config`
+--
+ALTER TABLE `system_config`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `email_config`
+--
+ALTER TABLE `email_config`
+  MODIFY `Id` int(145) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `google_recaptcha_api`
+--
+ALTER TABLE `google_recaptcha_api`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `system_config`
+--
+ALTER TABLE `system_config`
+  MODIFY `id` int(14) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
